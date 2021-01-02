6569e3581a6190592583d5247839aea33acd9dbc
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2021-01-02 22:53:29 +0900

    lein new duct route-guide

---
+++ src/route_guide/main.clj
@@ -0,0 +1,12 @@
+(ns route-guide.main
+  (:gen-class)
+  (:require [duct.core :as duct]))
+
+(duct/load-hierarchy)
+
+(defn -main [& args]
+  (let [keys     (or (duct/parse-keys args) [:duct/daemon])
+        profiles [:duct.profile/prod]]
+    (-> (duct/resource "route_guide/config.edn")
+        (duct/read-config)
+        (duct/exec-config profiles keys))))
